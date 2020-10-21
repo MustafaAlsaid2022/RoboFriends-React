8eff283d740271476c653e93edfa74e8595ae263
Alsaid Mustafa <musals003@vaxjo.se>
2020-10-21 16:57:03 +0200

    Updates

---
+++ precache-manifest.e7d4d20b81d4f7f96bc589e5f9bec7e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fee09661e63b867f0809e608790582",
+    "url": "/RoboFriends-React/index.html"
+  },
+  {
+    "revision": "41b302d0fc6bb8ebe540",
+    "url": "/RoboFriends-React/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6c28518fb63431c94df2",
+    "url": "/RoboFriends-React/static/css/main.eed7b251.chunk.css"
+  },
+  {
+    "revision": "41b302d0fc6bb8ebe540",
+    "url": "/RoboFriends-React/static/js/2.d25fa9f7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/RoboFriends-React/static/js/2.d25fa9f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c28518fb63431c94df2",
+    "url": "/RoboFriends-React/static/js/main.4c9ae050.chunk.js"
+  },
+  {
+    "revision": "64010e497f920ff2a9fe",
+    "url": "/RoboFriends-React/static/js/runtime-main.84f13b03.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/RoboFriends-React/static/media/SEGA.82e09f78.TTF"
+  }
+]);
